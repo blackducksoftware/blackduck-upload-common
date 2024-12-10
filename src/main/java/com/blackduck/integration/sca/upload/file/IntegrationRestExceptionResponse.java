772dd59545fad4f326933f9a4a8906dcc5a2b410
Dana Maxfield <danam@blackduck.com>
2024-12-10 10:09:40 -0500

    RDSQDRN-50 - Add missing copyright

--- src/main/java/com/blackduck/integration/sca/upload/file/IntegrationRestExceptionResponse.java
+++ src/main/java/com/blackduck/integration/sca/upload/file/IntegrationRestExceptionResponse.java
@@ -1,3 +1,10 @@
+/*
+ * blackduck-upload-common
+ *
+ * Copyright (c) 2024 Black Duck Software, Inc.
+ *
+ * Use subject to the terms and conditions of the Black Duck Software End User Software License and Maintenance Agreement. All rights reserved worldwide.
+ */
 package com.blackduck.integration.sca.upload.file;
 
 import java.io.InputStream;
