26c3af29970be56011555e313d36f01c1b0cd662
devmehtabd <devmehta@blackduck.com>
2025-12-01 13:06:10 -0500

    Add Xml body content for complete multipart request

--- src/main/java/com/blackduck/integration/sca/upload/file/XmlApiFileUploader.java
+++ src/main/java/com/blackduck/integration/sca/upload/file/XmlApiFileUploader.java
@@ -3,13 +3,28 @@ package com.blackduck.integration.sca.upload.file;
 import com.blackduck.integration.exception.IntegrationException;
 import com.blackduck.integration.rest.HttpMethod;
 import com.blackduck.integration.rest.HttpUrl;
+import com.blackduck.integration.rest.body.BodyContent;
+import com.blackduck.integration.rest.body.StringBodyContent;
 import com.blackduck.integration.rest.client.IntHttpClient;
 import com.blackduck.integration.rest.request.Request;
 import com.blackduck.integration.sca.upload.file.model.MultipartUploadFileMetadata;
 import com.blackduck.integration.sca.upload.file.model.MultipartUploadFilePart;
 import com.blackduck.integration.sca.upload.file.model.MultipartUploadStartRequestData;
 import com.blackduck.integration.sca.upload.file.model.MultipartUrlData;
+import org.apache.http.entity.ContentType;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamWriter;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.StringWriter;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
 
@@ -53,11 +68,40 @@ public class XmlApiFileUploader extends AbstractFileUploader {
         Map<String, String> finishHeaders = completeUploadUrl.getHeaders();
         HttpUrl requestUrl = new HttpUrl(completeUploadUrl.getUrl());
 
-        Request.Builder builder = new Request.Builder()
-                .url(requestUrl)
-                .headers(finishHeaders)
-                .method(HttpMethod.POST);
+        try {
+            StringWriter writer = new StringWriter();
+            XMLStreamWriter xmlWriter = XMLOutputFactory.newInstance().createXMLStreamWriter(writer);
 
-        return builder.build();
+            xmlWriter.writeStartElement("CompleteMultipartUpload");
+
+            for (Map.Entry<Integer, String> entry : tagOrderMap.entrySet()) {
+                xmlWriter.writeStartElement("Part");
+                xmlWriter.writeStartElement("PartNumber");
+                xmlWriter.writeCharacters(entry.getKey().toString());
+                xmlWriter.writeEndElement();
+                xmlWriter.writeStartElement("ETag");
+                xmlWriter.writeCharacters(entry.getValue());
+                xmlWriter.writeEndElement();
+                xmlWriter.writeEndElement();
+            }
+
+            xmlWriter.writeEndElement();
+            xmlWriter.writeEndDocument();
+            xmlWriter.close();
+
+            String xmlBody = writer.toString();
+
+            StringBodyContent bodyContent = new StringBodyContent(xmlBody, ContentType.APPLICATION_XML);
+
+            Request.Builder builder = new Request.Builder()
+                    .url(requestUrl)
+                    .headers(finishHeaders)
+                    .bodyContent(bodyContent)
+                    .method(HttpMethod.POST);
+
+            return builder.build();
+        } catch (Exception e) {
+            throw new IntegrationException("Error creating multipart upload URL", e);
+        }
     }
 }
